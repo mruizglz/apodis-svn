ee252e1e170e6e34cd796c9470410e7dfdc46a24
jmlopez <jmlopez@29bf9d6f-d319-3242-95cd-de4e3748e363>
2011-08-17 14:23:37 +0000

    Codigo convertido a double

--- Apodis2/tools/tools.h
+++ Apodis2/tools/tools.h
@@ -5,14 +5,14 @@
 
 typedef  struct {
     char name[CHARMAX];  //Name of signal. Acces path to JET BBDD
-    float Max;           //Maximum for normalization
-    float Min;           //Minimum for normalization
+    double Max;           //Maximum for normalization
+    double Min;           //Minimum for normalization
     int Npoints;         //Number of points for model window
     int nSamples;        //Number of samples of the signal
-    float *pData;        //Pointer to raw data
-    float *pTime;        //pointer to time for raw data signal
-    float *pTimeR;       //pointer to resampling times for nearest time window
-    float **pM;         //pointer to array of pointer of model windows
+    double *pData;        //Pointer to raw data
+    double *pTime;        //pointer to time for raw data signal
+    double *pTimeR;       //pointer to resampling times for nearest time window
+    double **pM;         //pointer to array of pointer of model windows
     int Normalize;        //the signal has to be Normalize
 
   } signal;
@@ -22,33 +22,33 @@ typedef struct {
   double bias;
   int coef_vector;
   int nvectors;
-  float **data;
-  float *alfa;
+  double **data;
+  double *alfa;
 } model;
 
 
 
-int IndexEvent( float *pBufferin, int n_samples, float Threshold, int type);
-float IntLin (float xi, float yi, float xf, float yf, float in);
-//int  resampling (int index, double resampling, float t0, float *pDataR, signal *wave, int Normalize);
-int  resampling (int index, double resampling, double t0, float *pDataR, signal *wave);
-float normalize (float Max, float Min, float data);
-int  ReadFloatTxt(char *path, float *buffer);
-float Mean (float *pData, int npoints);
+int IndexEvent( double *pBufferin, int n_samples, double Threshold, int type);
+double IntLin (double xi, double yi, double xf, double yf, double in);
+//int  resampling (int index, double resampling, double t0, double *pDataR, signal *wave, int Normalize);
+int  resampling (int index, double resampling, double t0, double *pDataR, signal *wave);
+double normalize (double Max, double Min, double data);
+int  ReadFloatTxt(char *path, double *buffer);
+double Mean (double *pData, int npoints);
 int ReadNormalizeTxt(char *path, int *buffer);
 int ReadModelTxt(char *path, char **buffer);
 void  N_vectors(char * path, int *nvectors, int *ncoefficients);
 void  M_values (char * path, model *Model);
-void  Read_M (char * path, float *M);
+void  Read_M (char * path, double *M);
 void Absolute ( double (*x)[2], double *result, int npoints);
 //void Absolute ( double *rea, double *imj, double *result, int npoints);
 double Desv (double *pData, int npoints);
-//void distance (float (*input)[3], model *Model, int nModels, float *D);
-double distance (float *input, model *Model);
+//void distance (double (*input)[3], model *Model, int nModels, double *D);
+double distance (double *input, model *Model);
 int bufferFree (int *indice, int nModelos);
-void salvaOutput( float *input, char *path);
+void salvaOutput( double *input, char *path);
 int iwindow_1 (int *indice, int nModelos);
-double prod_vect (float *input, float *Coeficientes, float bias, int nCoeficientes);
+double prod_vect (double *input, double *Coeficientes, double bias, int nCoeficientes);
 int iwindow1 (int *indice, int nModelos);
 
 
